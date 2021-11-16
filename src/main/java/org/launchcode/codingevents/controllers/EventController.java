f9ce5260e5e86ed829dbeb069614d4441adb0437
kat-billings <katbillings235@gmail.com>
2021-11-16 02:36:10 -0600

    completed chapter 11 exercises

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
@@ -16,9 +17,19 @@ public class EventController {
 
     private static List<String> events = new ArrayList<>();
 
+//    @GetMapping
+//    public String displayAllEvents(Model model){
+//        model.addAttribute("events", events);
+//        return "events/index";
+//    }
+
     @GetMapping
     public String displayAllEvents(Model model){
-        model.addAttribute("events", events);
+        HashMap<String,String> events = new HashMap<>();
+        events.put("Menteaship","A fun meetup for connecting with mentors");
+        events.put("Code With Pride","A fun meetup sponsored by LaunchCode");
+        events.put("Javascripty", "An imaginary meetup for Javascript developers");
+        model.addAttribute("events",events);
         return "events/index";
     }
 
