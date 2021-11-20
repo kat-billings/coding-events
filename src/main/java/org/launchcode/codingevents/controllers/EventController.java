c48eef6a407bbdb8a9e7623641fceca230557a6a
kat-billings <katbillings235@gmail.com>
2021-11-20 15:39:12 -0600

    completed chapter 13 and 14 reading

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -2,9 +2,13 @@ package org.launchcode.codingevents.controllers;
 
 import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
+import org.launchcode.codingevents.models.EventType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("events")
@@ -12,6 +16,7 @@ public class EventController {
 
     @GetMapping
     public String displayAllEvents(Model model){
+        model.addAttribute("title","All Events");
         model.addAttribute("events", EventData.getAll());
         return "events/index";
     }
@@ -28,12 +33,19 @@ public class EventController {
 
     // lives at /events/create
     @GetMapping("create")
-    public String renderCreateEventForm(){
+    public String renderCreateEventForm(Model model){
+        model.addAttribute("title","Create Event");
+        model.addAttribute(new Event());
+        model.addAttribute("types",EventType.values());
         return "events/create";
     }
 
     @PostMapping("create")
-    public String createEvent(@ModelAttribute Event newEvent){
+    public String createEvent(@ModelAttribute @Valid Event newEvent, Errors errors, Model model){
+        if((errors.hasErrors())){
+            model.addAttribute("title","Create Event");
+            return "events/create";
+        }
         EventData.add(newEvent);
         return "redirect:";
     }
@@ -59,7 +71,7 @@ public class EventController {
     public String displayEditForm(Model model, @PathVariable int eventId) {
         Event event = EventData.getById(eventId);
         model.addAttribute("event",event);
-        model.addAttribute("title","Edit Event " + event.getName() + " (" + event.getId() + ")");
+        model.addAttribute("title","Edit Event " + event.getName() + " (ID: " + event.getId() + ")");
         return "events/edit";
     }
 
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,44 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
 
     private int id;
     private static int nextId = 1;
-    private String name;
-    private String description;
 
-    public Event(String name, String description) {
-        this.name = name;
-        this.description = description;
+    @Size(min=3,max=50, message = "Name must be between 3 and 50 characters.")
+    @NotBlank(message = "Name required.")
+    private String name;
+    @Size(max=500, message = "Description too long.")
+    private String description;
+    @NotBlank(message = "Email required.")
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
+    @NotBlank(message = "Location required.")
+    private String location;
+    @AssertTrue(message = "Registration must be required.")
+    private boolean registrationRequired;
+    @Positive(message = "Number of attendees must be over 0.")
+    private int attendees;
+
+    private EventType type;
+
+    public Event(){
         this.id = nextId;
         nextId++;
+    }
+
+    public Event(String name, String description, String contactEmail, String location, boolean registrationRequired, int attendees, EventType type) {
+        this();
+        this.name = name;
+        this.description = description;
+        this.contactEmail = contactEmail;
+        this.location = location;
+        this.registrationRequired = registrationRequired;
+        this.attendees = attendees;
+        this.type = type;
     }
 
     public String getName() {
@@ -32,8 +57,48 @@ public class Event {
         this.description = description;
     }
 
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public boolean isRegistrationRequired() {
+        return registrationRequired;
+    }
+
+    public void setRegistrationRequired(boolean registrationRequired) {
+        this.registrationRequired = registrationRequired;
+    }
+
     public int getId() {
         return id;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+
+    public int getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(int attendees) {
+        this.attendees = attendees;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     @Override
